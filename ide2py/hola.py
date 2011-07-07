729f48929219a648e2dbde06da71707e798a51f9
Mariano Reingart <reingart@gmail.com>
2011-07-06 21:30:39 -0300

    Modified sample python file for testing

--- ide2py/hola.py
+++ ide2py/hola.py
@@ -1,15 +1,32 @@
 # sample file to test shell & debugger
 
+def factorial(n):
+    """This is the "factorial" function
+    >>> factorial(5)
+    120
+    >>> factorial(None)
+    0
+    
+    """
+    if n is None:
+        raise RuntimeError("test exception")
+    result = 1
+    factor = 2
+    while factor <= n:
+        result *= factor
+        factor += 1
+    return result
 
 def main(h="hola"):
     a = h
     n = raw_input("nombre?")
 
-    n = n + "ldasf"
+    n = n+"ldasf"
 
     if n:
         print a, n
+        
 
 #asdf = hola
-
-main(h=1)
+if __name__=="__main__":
+    main(h=1)
