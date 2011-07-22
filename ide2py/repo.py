5603e2fa89ca146be05f169e1975a55fedd122a3
Mariano Reingart <reingart@gmail.com>
2011-07-22 04:12:53 -0300

    Repository: Adding contextual menu and diff support

--- ide2py/repo.py
+++ ide2py/repo.py
@@ -42,7 +42,51 @@ class RepoMixin(object):
         self.repo_tree.Bind(wx.EVT_LEFT_DCLICK, self.OnRepoLeftDClick)
         #self.repo_tree.Bind(wx.EVT_RIGHT_DOWN, self.OnRightDown)
         #self.repo_tree.Bind(wx.EVT_RIGHT_UP, self.OnRightUp)
+        self.repo_tree.Bind(wx.EVT_CONTEXT_MENU, self.OnRepoContextMenu)
+
         self.Connect(-1, -1, EVT_REPO_ID, self.OnRepoEvent)
+
+#        tb5 = self.CreateRepoToolbar()
+#        self._mgr.AddPane(tb5, wx.aui.AuiPaneInfo().
+#                          Name("tb5").Caption("Repository Toolbar").
+#                          ToolbarPane().Top().Row(1).Position(4).
+#                          LeftDockable(False).RightDockable(False).CloseButton(True))
+#        self._mgr.Update()
+
+        self.CreateRepoMenu()
+
+
+    def CreateRepoMenu(self):
+
+        m = self.repo_menu = wx.Menu()
+        menus = ['Update', 'Commit', 'Diff', 'Revert', 'Push', 'Pull']
+        methods = [self.OnRepoUpdate, self.OnRepoCommit, self.OnRepoDiff,
+            self.OnRepoRevert, self.OnRepoPush, self.OnRepoPull]
+        for item_name, item_method in zip(menus, methods):
+            item_id = wx.NewId()
+            m.Append(item_id, item_name)
+            self.Bind(wx.EVT_MENU, item_method, id=item_id)
+
+
+    def CreateRepoToolbar(self):
+        #ID_ADD, ID_DEL, ID_COMMIT = [wx.NewId() for i in range(3)]
+        tb4 = wx.ToolBar(self, -1, wx.DefaultPosition, wx.DefaultSize,
+                         wx.TB_FLAT | wx.TB_NODIVIDER)
+        tsize = wx.Size(16, 16)
+        tb4.SetToolBitmapSize(tsize)
+
+        GetBmp = wx.ArtProvider.GetBitmap
+
+        tb4.AddSimpleTool(
+            wx.ID_UNDO, GetBmp(wx.ART_GO_UP, wx.ART_TOOLBAR, tsize), "Commit")
+        tb4.AddSimpleTool(
+            wx.ID_UNDO, GetBmp(wx.ART_GO_DOWN, wx.ART_TOOLBAR, tsize), "Update")
+
+        tb4.AddSimpleTool(
+            wx.ID_UNDO, GetBmp(wx.ART_HELP_SIDE_PANEL, wx.ART_TOOLBAR, tsize), "Diff")
+        
+        tb4.Realize()
+        return tb4
 
     def CreateRepoTreeCtrl(self, path):
         self.repo_tree = tree = wx.TreeCtrl(self, -1, wx.Point(0, 0), wx.Size(160, 250),
@@ -99,13 +143,17 @@ class RepoMixin(object):
                 node = current[basename]
                 tree.SetItemImage(node, icons[st], wx.TreeItemIcon_Normal)
 
-
-    def OnRepoLeftDClick(self, event):
-        pt = event.GetPosition();
-        item, flags = self.repo_tree.HitTest(pt)
+    def get_selected_filename(self, event):
+        item = self.repo_tree.GetSelection() 
         if item:
             filename = self.repo_tree.GetPyData(item)
-            print filename
+            return filename
+
+
+    def OnRepoLeftDClick(self, event):
+        filename = self.get_selected_filename(event)
+        if filename:
+            filename = self.repo_tree.GetPyData(item)
             self.DoOpen(filename)
         event.Skip()
 
@@ -115,4 +163,33 @@ class RepoMixin(object):
             print "OnRepoEvent", filename, action
             self.RefreshRepo([filename])
 
+    def OnRepoContextMenu(self, event):
+        self.PopupMenu(self.repo_menu)
+        #menu.Destroy()
+
+    def OnRepoUpdate(self, event):
+        pass
+
+    def OnRepoCommit(self, event):
+        pass
+
+    def OnRepoDiff(self, event):
+        filename = self.get_selected_filename(event)
+        if filename:
+            self.DoDiff(filename)
+
+    def OnRepoRevert(self, event):
+        pass
+    
+    def OnRepoPush(self, event):
+        pass
+    
+    def OnRepoPull(self, event):
+        pass
+
+    def DoDiff(self, filename):
+        old = self.repo.cat(filename)
+        new = open(filename, "U").read()
+        from wxpydiff import PyDiff
+        PyDiff(None, 'wxPyDiff', "repository", filename, old, new)
 
