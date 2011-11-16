b329ec9b03d1f10b4824c94a2613ca9b26390594
Mariano Reingart <reingart@gmail.com>
2011-11-15 23:54:44 -0300

    updated simplejsonrpc with the one of web2py

--- ide2py/simplejsonrpc.py
+++ ide2py/simplejsonrpc.py
@@ -15,10 +15,13 @@
 __author__ = "Mariano Reingart (reingart@gmail.com)"
 __copyright__ = "Copyright (C) 2011 Mariano Reingart"
 __license__ = "LGPL 3.0"
-__version__ = "0.01"
+__version__ = "0.04"
 
 
-import urllib2
+import urllib
+from xmlrpclib import Transport, SafeTransport
+from cStringIO import StringIO
+import random
 import sys
 try:
     import gluon.contrib.simplejson as json     # try web2py json serializer 
@@ -27,6 +30,7 @@ except ImportError:
         import json                             # try stdlib (py2.6)
     except:
         import simplejson as json               # try external module
+
 
 class JSONRPCError(RuntimeError):
     "Error object for remote procedure call fail"
@@ -39,14 +43,63 @@ class JSONRPCError(RuntimeError):
         return self.__unicode__().encode("ascii","ignore")        
 
 
-class JSONRPCClient(object):
+class JSONDummyParser:
+    "json wrapper for xmlrpclib parser interfase"
+    def __init__(self):
+        self.buf = StringIO()
+    def feed(self, data):
+        self.buf.write(data)
+    def close(self):
+        return self.buf.getvalue()
+
+
+class JSONTransportMixin:
+    "json wrapper for xmlrpclib transport interfase"
+    
+    def send_content(self, connection, request_body):
+        connection.putheader("Content-Type", "application/json")
+        connection.putheader("Content-Length", str(len(request_body)))
+        connection.endheaders()           
+        if request_body:
+            connection.send(request_body)
+        # todo: add gzip compression
+
+    def getparser(self):
+        # get parser and unmarshaller
+        parser = JSONDummyParser()
+        return parser, parser
+
+
+class JSONTransport(JSONTransportMixin, Transport):
+    pass
+
+class JSONSafeTransport(JSONTransportMixin, SafeTransport):
+    pass
+
+    
+class ServerProxy(object):
     "JSON RPC Simple Client Service Proxy"
-    def __init__(self, location=None, exceptions=True, trace=False, timeout=60):
-        self.location = location        # server location (url)
-        self.trace = trace              # show debug messages
-        self.exceptions = exceptions    # raise errors? (JSONRPCError)
-        self.timeout = timeout
+
+    def __init__(self,  uri, transport=None, encoding=None, verbose=0):
+        self.location = uri             # server location (url)
+        self.trace = verbose            # show debug messages
+        self.exceptions = True          # raise errors? (JSONRPCError)
+        self.timeout = None
         self.json_request = self.json_response = ''
+
+        type, uri = urllib.splittype(uri)
+        if type not in ("http", "https"):
+            raise IOError, "unsupported JSON-RPC protocol"
+        self.__host, self.__handler = urllib.splithost(uri)
+
+        if transport is None:
+            if type == "https":
+                transport = JSONSafeTransport()
+            else:
+                transport = JSONTransport()
+        self.__transport = transport
+        self.__encoding = encoding
+        self.__verbose = verbose                
 
     def __getattr__(self, attr):
         "pseudo method that can be called"
@@ -56,56 +109,42 @@ class JSONRPCClient(object):
         "JSON RPC communication (method invocation)"
 
         # build data sent to the service
-        data = {'id': 1, 'method': method, 'params': args, }
-        body = json.dumps(data)
-        headers = {'Content-type': 'text/x-json; charset="UTF-8"',
-                   'Content-length': str(len(body)),}
+        request_id = random.randint(0, sys.maxint)
+        data = {'id': request_id, 'method': method, 'params': args, }
+        request = json.dumps(data)
 
-        # make HTTP request
-        req = urllib2.Request(self.location)
-        for key, value in headers.items():
-            req.add_header(key, value)
-        req.add_data(body)
-
-        if self.trace:
-            print "-"*80
-            print "POST %s" % self.location
-            print '\n'.join(["%s: %s" % (k,v) for k,v in headers.items()])
-            print u"\n%s" % body
-
-        # send request and receive result (timeout only available on py2.6):
-        if sys.version_info[0:2] > (2,5):
-            f = urllib2.urlopen(req, timeout=self.timeout)
-        else:
-            f = urllib2.urlopen(req)
-        content = f.read()
-        
+        # make HTTP request (retry if connection is lost)
+        response = self.__transport.request(
+            self.__host,
+            self.__handler,
+            request,
+            verbose=self.__verbose
+            )
+      
         # store plain request and response for further debugging
-        self.json_request = body
-        self.json_response = content
-
-        if self.trace: 
-            print '\n', f.info(), '\n', content, '\n', "="*80
+        self.json_request = request
+        self.json_response = response
 
         # parse json data coming from service 
         # {'version': '1.1', 'id': id, 'result': result, 'error': None}
-        response = json.loads(content)
+        response = json.loads(response)
+        
+        if response['id'] != request_id:
+            raise JSONRPCError(0, "JSON Request ID != Response ID")
 
-        self.error = response['error']
+        self.error = response.get('error', {})
         if self.error and self.exceptions:
-            # {'name': 'JSONRPCError', 'code': code, 'message': message}
-            raise JSONRPCError(self.error['code'], self.error['message'])
-            
+            raise JSONRPCError(self.error.get('code', 0), self.error.get('message', ''))
+
         return response.get('result')
 
 
-ServiceProxy = JSONRPCClient
+ServiceProxy = ServerProxy
+
 
 if __name__ == "__main__":
     # basic tests:
-    client = JSONRPCClient(
-                location="http://localhost:8000/psp2py/services/call/jsonrpc",
-                exceptions=True, trace=True,
-                )
+    location = "http://www.web2py.com.ar/webservices/sample/call/jsonrpc"
+    client = ServerProxy(location, verbose='--verbose' in sys.argv,)
     print client.add(1, 2)
 
