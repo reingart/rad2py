dbe3c8a660949a1165244b80542b707535a4e954
Mariano Reingart <reingart@gmail.com>
2012-01-13 03:35:10 -0300

    Qdb: added basic CLI frontend (Cmd)

--- ide2py/qdb.py
+++ ide2py/qdb.py
@@ -14,7 +14,7 @@ import bdb
 import os
 import sys
 import traceback
-import json
+import cmd
 
 
 def log(func):
@@ -35,13 +35,34 @@ class Qdb(bdb.Bdb):
         self.waiting = False
         self.pipe = pipe # for communication
         self.start_continue = True # continue on first run
+        self._wait_for_mainpyfile = False
+
+    # Override Bdb methods
 
     @log
+    def user_call(self, frame, argument_list):
+        """This method is called when there is the remote possibility
+        that we ever need to stop in this function."""
+        if self._wait_for_mainpyfile:
+            return
+        if self.stop_here(frame):
+            print >>self.stdout, '--Call--'
+            self.interaction(frame, None)
+            
+    @log
     def user_line(self, frame):
+        """This function is called when we stop or break at this line."""
+        if self._wait_for_mainpyfile:
+            if (self.mainpyfile != self.canonic(frame.f_code.co_filename)
+                or frame.f_lineno<= 0):
+                return
+            self._wait_for_mainpyfile = 0
         self.interaction(frame)
 
     @log
     def user_exception(self, frame, info):
+        if self._wait_for_mainpyfile:
+            return
         print info
         extype, exvalue, trace = info
         # pre-process stack trace as it isn't pickeable (cannot be sent pure)
@@ -51,30 +72,44 @@ class Qdb(bdb.Bdb):
         self.interaction(frame, info)
 
     @log
-    def Run(self, code, interp=None, *args, **kwargs):
+    def run(self, code, interp=None, *args, **kwargs):
         try:
             self.interp = interp
             self.interacting = self.start_continue and 1 or 2
-            return self.run(code, *args, **kwargs)
+            return bdb.Bdb.run(self, code, *args, **kwargs)
         finally:
             self.interacting = 0
 
     @log
-    def RunCall(self, function, interp=None, *args, **kwargs):
+    def runcall(self, function, interp=None, *args, **kwargs):
         try:
             self.interp = interp
             self.interacting = self.start_continue and 1 or 2
-            return self.runcall(function, *args, **kwargs)
+            return bdb.Bdb.runcall(self, function, *args, **kwargs)
         finally:
             self.interacting = 0
+
+    @log
+    def _runscript(self, filename):
+        # The script has to run in __main__ namespace (clear it)
+        import __main__
+        __main__.__dict__.clear()
+        __main__.__dict__.update({"__name__"    : "__main__",
+                                  "__file__"    : filename,
+                                  "__builtins__": __builtins__,
+                                 })
+
+        # avoid stopping before we reach the main script 
+        self._wait_for_mainpyfile = 1
+        self.mainpyfile = self.canonic(filename)
+        self._user_requested_quit = 0
+        statement = 'execfile( "%s")' % filename
+        self.run(statement)
+
+    # General interaction function
 
     @log
     def interaction(self, frame, info=None):
-        # first callback (Run)?, just continue...
-        if self.interacting == 1:
-            self.interacting += 1
-            self.set_continue()
-            return
         code, lineno = frame.f_code, frame.f_lineno
         filename = code.co_filename
         basename = os.path.basename(filename)
@@ -95,6 +130,7 @@ class Qdb(bdb.Bdb):
         ## frame.f_globals
         try:
             while self.waiting:
+                self.pipe.send({'method': 'Interaction', 'args': ()})
                 print ">>>",
                 request = self.pipe.recv()
                 print request
@@ -114,33 +150,35 @@ class Qdb(bdb.Bdb):
             self.waiting = False
         self.frame = None
 
+    # Command definitions, called by interaction()
+
     @log
-    def Continue(self):
+    def do_continue(self):
         self.set_continue()
         self.waiting = False
 
     @log
-    def Step(self):
+    def do_step(self):
         self.set_step()
         self.waiting = False
 
     @log
-    def StepReturn(self):
+    def do_return(self):
         self.set_return(self.frame)
         self.waiting = False
 
     @log
-    def Next(self):
+    def do_next(self):
         self.set_next(self.frame)
         self.waiting = False
 
     @log
-    def Quit(self):
+    def do_quit(self):
         self.set_quit()
         self.waiting = False
 
     @log
-    def Jump(self, lineno):
+    def do_jump(self, lineno):
         arg = int(lineno)
         try:
             self.frame.f_lineno = arg
@@ -149,15 +187,15 @@ class Qdb(bdb.Bdb):
             return False
 
     @log
-    def SetBreakpoint(self, filename, lineno, temporary=0):
+    def do_set_breakpoint(self, filename, lineno, temporary=0):
         self.set_break(self.canonic(filename), lineno, temporary)
 
     @log
-    def ClearBreakpoint(self, filename, lineno):
+    def do_clear_breakpoint(self, filename, lineno):
         self.clear_break(filename, lineno)
 
     @log
-    def ClearFileBreakpoints(self, filename):
+    def do_clear_file_breakpoints(self, filename):
         self.clear_all_file_breaks(filename)
 
     @log
@@ -168,16 +206,21 @@ class Qdb(bdb.Bdb):
             print '*** DO_CLEAR failed', err
 
     @log
-    def inspect(self, arg):
-        try:
-            return eval(arg, self.frame.f_globals,
-                        self.frame.f_locals)
-        except:
-            t, v = sys.exc_info()[:2]
-            if isinstance(t, str):
-                exc_type_name = t
-            else: exc_type_name = t.__name__
-            return '*** %s: %s' % (exc_type_name, repr(v))
+    def do_inspect(self, arg):
+        return eval(arg, self.frame.f_globals,
+                    self.frame.f_locals)
+
+    def do_exec(self, arg):
+        code = compile(arg + '\n', '<stdin>', 'single')
+        exec code in self.frame.f_globals, self.frame.f_locals
+
+    def displayhook(self, obj):
+        """Custom displayhook for the do_exec which prevents
+        assignment of the _ variable in the builtins.
+        """
+        # reproduce the behavior of the standard displayhook, not printing None
+        if obj is not None:
+            self.pipe.send({'method': 'display_hook', 'args':  repr(obj)})
 
     @log
     def reset(self):
@@ -208,8 +251,8 @@ class Qdb(bdb.Bdb):
         self.interaction(frame)
 
 
-class Pipe(object):
-    "Simulated pipe for threads"
+class QueuePipe(object):
+    "Simulated pipe for threads (using two queues)"
     
     def __init__(self, name, in_queue, out_queue):
         self.__name = name
@@ -229,12 +272,13 @@ class Pipe(object):
         
 
 
-if __name__ == '__main__':
+def test():
     def f(pipe):
         print "creating debugger"
         qdb = Qdb(pipe=pipe)
         print "set trace"
 
+        my_var = "Mariano!"
         qdb.set_trace()
         print "hello world!"
         print "good by!"
@@ -244,22 +288,161 @@ if __name__ == '__main__':
         from multiprocessing import Process, Pipe
         pipe, child_conn = Pipe()
         p = Process(target=f, args=(child_conn,))
+
     else:
         from threading import Thread
         from Queue import Queue
         parent_queue, child_queue = Queue(), Queue()
-        pipe = Pipe("parent", parent_queue, child_queue)
-        child_conn = Pipe("child", child_queue, parent_queue)
+        pipe = QueuePipe("parent", parent_queue, child_queue)
+        child_conn = QueuePipe("child", child_queue, parent_queue)
         p = Thread(target=f, args=(child_conn,))
     
     p.start()
     i = 0
+
     while 1:
         print "<<<", pipe.recv()
         raw_input()
-        msg = {'method': 'Step', 'args': (), 'id': i}
+        msg = {'method': 'do_step', 'args': (), 'id': i}
         pipe.send(msg)
         i += 1
 
     p.join()
+
+
+class Cli(cmd.Cmd):
+    "Qdb Front-end command line interface"
     
+    def __init__(self, pipe, completekey='tab', stdin=None, stdout=None, skip=None):
+        cmd.Cmd.__init__(self, completekey, stdin, stdout)
+        self.i = 0
+        self.pipe = pipe
+
+    def attach(self):
+        while 1:
+            msg = self.pipe.recv()
+            print msg
+            if msg.get('method') == 'Interaction':
+                self.interaction()
+
+    def interaction(self):
+        self.cmdloop()
+
+    def postcmd(self, stop, line):
+        return not line.startswith("h") # stop
+
+    def call(self, method, *args):
+        msg = {'method': method, 'args': args, 'id': self.i}
+        print msg
+        self.pipe.send(msg)
+        self.i += 1
+
+    do_h = cmd.Cmd.do_help
+
+    def do_s(self, arg):
+        "Execute the current line, stop at the first possible occasion"
+        self.call('do_step')
+        
+    def do_n(self, arg):
+        "Execute the current line, do not stop at function calls"
+        self.call('do_next')
+
+    def do_c(self, arg): 
+        "Continue execution, only stop when a breakpoint is encountered."
+        self.call('do_continue')
+        
+    def do_r(self, arg): 
+        "Continue execution until the current function returns"
+        self.call('do_return')
+
+    def do_j(self, arg): 
+        "Set the next line that will be executed."
+        self.call('do_jump', arg)
+
+    def do_q(self, arg):
+        "Quit from the debugger. The program being executed is aborted."
+        self.call('do_quit')
+    
+    def do_p(self, arg):
+        "Inspect the value of the expression"
+        self.call('do_inspect', arg)
+
+    def default(self, line):
+        "Default command"
+        if line[:1] == '!':
+            line = line[1:]
+            self.call('do_exec', line)
+        else:
+            print "*** Unknown command: ", l
+
+
+def connect(host="localhost", port=6000):
+    "Connect to a running debugger backend"
+    
+    address = (host, port)
+    from multiprocessing.connection import Client
+
+    conn = Client(address, authkey='secret password')
+    try:
+        Cli(conn).attach()
+    finally:
+        conn.close()
+
+
+def main():
+    "Debug a script and accept a remote frontend"
+    
+    if not sys.argv[1:] or sys.argv[1] in ("--help", "-h"):
+        print "usage: pdb.py scriptfile [arg] ..."
+        sys.exit(2)
+
+    mainpyfile =  sys.argv[1]     # Get script filename
+    if not os.path.exists(mainpyfile):
+        print 'Error:', mainpyfile, 'does not exist'
+        sys.exit(1)
+
+    del sys.argv[0]         # Hide "pdb.py" from argument list
+
+    # Replace pdb's dir with script's dir in front of module search path.
+    sys.path[0] = os.path.dirname(mainpyfile)
+
+    from multiprocessing.connection import Listener
+    address = ('localhost', 6000)     # family is deduced to be 'AF_INET'
+    listener = Listener(address, authkey='secret password')
+    print "waiting for connection", address
+    conn = listener.accept()
+    print 'connection accepted from', listener.last_accepted
+
+    # create the backend
+    qdb = Qdb(conn)
+    try:
+        print "running", mainpyfile
+        qdb._runscript(mainpyfile)
+        print "The program finished"
+    except SystemExit:
+        # In most cases SystemExit does not warrant a post-mortem session.
+        print "The program exited via sys.exit(). Exit status: ",
+        print sys.exc_info()[1]
+    except:
+        traceback.print_exc()
+        print "Uncaught exception. Entering post mortem debugging"
+        print "Running 'cont' or 'step' will restart the program"
+        t = sys.exc_info()[2]
+        ##qdb.interaction(None, t)
+        print "Post mortem debugger finished. The " + mainpyfile + \
+              " will be restarted"
+
+    conn.close()
+    listener.close()
+
+
+# When invoked as main program, invoke the debugger on a script
+if __name__ == '__main__':
+    #test()
+    if not sys.argv[1:]:
+        connect()
+    else:
+        # reimport as global __main__ namespace is destroyed
+        import qdb
+        qdb.main()
+
