67ddb06514b607f70c639c7be5888c7d1035f0cd
Mariano Reingart <reingart@gmail.com>
2013-01-08 04:20:07 -0300

    debugger: removed locks, semaphore, event, action queue, etc. (finishes fe8b044e7833)

--- ide2py/debugger.py
+++ ide2py/debugger.py
@@ -9,7 +9,6 @@ __license__ = "GPL 3.0"
 
 # based on idle, inspired by pythonwin implementation
 
-from threading import Thread, Event, Lock, Semaphore
 from Queue import Queue
 from multiprocessing.connection import Client
 import compiler
@@ -62,41 +61,36 @@ class Debugger(qdb.Frontend):
 
     def __init__(self, gui=None, pipe=None):
         qdb.Frontend.__init__(self, pipe)
-        self.interacting = Event()  # flag to 
-        self.done = Event()         # flag to block for user interaction
-        self.attached = Event()     # flag to block waiting for remote side
+        self.interacting = False    # flag to signal user interaction
+        self.attached = False       # flag to signal remote side availability
         self.gui = gui              # wx window for callbacks
         self.post_event = True      # send event to the GUI
         self.start_continue = True  # continue on first run
         self.rawinput = None
-        self.actions = Queue()
-        self.mutex = Lock()         # critical section protection (comm channel)
-        self.access = Semaphore()   # limit to 1 interaction command to use the channel
         self.filename = self.lineno = None
         self.unrecoverable_error = False
         self.pipe = None
         wx.GetApp().Bind(wx.EVT_IDLE, self.OnIdle) # schedule debugger execution
-        print "Binded!"
 
 
     def OnIdle(self, event):
         try:
-            if self.attached.is_set():
+            if self.attached:
                 while self.pipe.poll():
                     self.run()
-                self.push_actions()
-
         except EOFError:
             print "DEBUGGER disconnected..."
             self.detach()
         except IOError, e:
             print "DEBUGGER connection exception:", e
-        except:
+            self.detach()
+        except Exception, e:
+            # print the exception message and close (avoid recursion)
             print "DEBUGGER exception", e
+            import traceback
+            traceback.traceback.print_exc()
             import sys;
             sys.exit()
-        #finally:
-            #self.detach()
 
     def init(self, cont=False):
         self.start_continue = cont
@@ -108,57 +102,65 @@ class Debugger(qdb.Frontend):
         print "DEBUGGER waiting for connection to", self.address
         self.pipe = LoggingPipeWrapper(Client(self.address, authkey=self.authkey))
         print "DEBUGGER connected!"
-        self.attached.set()
+        self.attached = True
     
     def detach(self):
-        self.attached.clear()
+        self.attached = False
         if self.pipe:
             self.pipe.close()
 
     def is_remote(self):
-        return (self.attached.is_set() and 
+        return (self.attached and 
                 self.address[0] not in ("localhost"))
-
-    def call(self, method, *args):
-        "Schedule a call for further execution by the thread"
-        try:
-            self.actions.put(lambda: qdb.Frontend.call(self, method, *args))
-        except Exception, e:
-            # TODO: proper notification
-            print e
-
-    def push_actions(self):
-        "Execute scheduled actions"
-        ret = None
-        while not self.actions.empty():
-            if self.interacting.is_set():
-                self.interacting.clear()
-                # clean current line
-                if self.gui and self.post_event:
-                    wx.PostEvent(self.gui, DebugEvent(EVT_DEBUG_ID, (None, None, None, None)))
-            action = self.actions.get()
-            try:
-                ret = action()
-            finally:
-                self.actions.task_done()
-        return ret
 
     def check_interaction(fn):
         "Decorator for mutually exclusive functions"
         def check_fn(self, *args, **kwargs):
-            if not self.interacting.is_set():
+            if not self.interacting or not self.pipe or not self.attached:
                 wx.Bell()
+                self.gui.ShowInfoBar("not interacting!", 
+                                     flags=wx.ICON_WARNING, key="debugger") 
             else:
-                if self.mutex.acquire(False):
-                    try:
-                        if self.access.acquire(False):
-                            fn(self, *args, **kwargs)
-                        else:
-                            print "cannot interact, semaphore hold"
-                    finally:
-                        self.mutex.release()
+                self.interacting = False
+                # interaction is done, clean current line marker
+                wx.PostEvent(self.gui, DebugEvent(EVT_DEBUG_ID, (None, None, None, None)))
+                return fn(self, *args, **kwargs)
         return check_fn
     
+    def force_interaction(fn):
+        "Decorator for functions that need to break immediately"
+        def check_fn(self, *args, **kwargs):
+            print "force interaction!!", fn.func_name, args, kwargs
+            # only send if debugger is connected and attached
+            if not self.pipe or not self.attached:
+                return False
+            # check if interaction is banned (i.e. readline!)
+            if self.interacting is None:
+                wx.Bell()
+                return
+            # do not send GUI notifications (to not alter the focus)
+            self.post_event = False
+            # if no interaction yet, send an interrupt (step on the next stmt)
+            if not self.interacting:
+                self.interrupt()
+                cont = True
+            else:
+                cont = False
+            # wait for interaction:
+            while not self.interacting:
+                # allow wx process some events 
+                wx.SafeYield()      # safe = user input "disabled"
+                self.OnIdle(None)   # force pipe processing
+            # send the method request                
+            ret = fn(self, *args, **kwargs)
+            # if interrupted, send a continue to resume
+            if cont:
+                self.post_event = True
+                self.interacting = False
+                self.do_continue()
+            return True
+        return check_fn
+
     def startup(self):
         # notification sent by _runscript before Bdb.run
         print "loading breakpoints...."
@@ -169,13 +171,10 @@ class Debugger(qdb.Frontend):
         qdb.Frontend.startup(self)
 
     def interaction(self, filename, lineno, line, **context):
-        self.done.clear()
-        self.interacting.set()
+        self.interacting = True
         try:
             if self.start_continue:
-                print "continuing..."
                 self.Continue()
-                self.push_actions()
                 self.start_continue = None
                 return
                 
@@ -186,34 +185,25 @@ class Debugger(qdb.Frontend):
                 self.orig_line = line.strip().strip("\r").strip("\n")
                 self.lineno = lineno
                 if self.gui and self.post_event:
-                    # we are in other thread so send the event to main thread
+                    # send the event to mark the current line
                     wx.PostEvent(self.gui, DebugEvent(EVT_DEBUG_ID, 
                                                       (filename, lineno, context, line)))
                 else:
                     # ignore this (async command) and reenable notifications
                     self.post_event = True
-
-            # wait user events: done is a threading.Event (set by the main thread)
-            ##self.done.wait()
-            
-            # execute user action scheduled by main thread
-            ##self.push_actions()
-
-
         finally:
-            #self.interacting.clear()
-            self.access.release()
+            pass
             
     def write(self, text):
         wx.PostEvent(self.gui, DebugEvent(EVT_WRITE_ID, text))
 
     def readline(self):
         # "raw_input" should be atomic and uninterrupted
-        self.mutex.acquire()
         try:
+            self.interacting = None
             return self.gui.Readline()
         finally:
-            self.mutex.release()
+            self.interacting = False
 
     def exception(self, *args):
         "Notify that a user exception was raised in the backend"
@@ -238,9 +228,6 @@ class Debugger(qdb.Frontend):
             dlg.Destroy()
             if quit:
                 self.do_quit()
-                self.done.set()
-            # free semaphore (no interaction command will be sent)
-            self.access.release()
             return False
         # check current text source code against running code
         if self.lineno is not None and self.orig_line != curr_line:
@@ -251,7 +238,6 @@ class Debugger(qdb.Frontend):
                 self.do_exec(curr_line)
                 print "executed", curr_line
                 self.do_jump(self.lineno+1)
-                ret = self.push_actions()
                 print "jump", ret
                 if not ret:
                     raise RuntimeError("Cannot jump to ignore modified line!")
@@ -263,8 +249,6 @@ class Debugger(qdb.Frontend):
                        wx.OK | wx.ICON_EXCLAMATION)
                 dlg.ShowModal()
                 dlg.Destroy()
-                # free semaphore (no interaction command will be sent)
-                self.access.release()
                 return False
         return True
 
@@ -277,40 +261,33 @@ class Debugger(qdb.Frontend):
                 # set a temp breakpoint (continue to...)
                 self.set_burst(2)
                 self.do_set_breakpoint(filename, lineno, temporary=1)
-                self.push_actions()
             self.do_continue()
-            self.done.set()
 
     @check_interaction
     def Step(self):
         if self.check_running_code():
             self.do_step()
-            self.done.set()
 
     @check_interaction
     def StepReturn(self):
         if self.check_running_code():
             self.do_return()
-            self.done.set()
 
     @check_interaction
     def Next(self):
         if self.check_running_code():
             self.do_next()
-            self.done.set()
 
-    @check_interaction
+    @force_interaction
     def Quit(self):
         self.do_quit()
-        self.done.set()
 
     @check_interaction
     def Jump(self, lineno):
         self.do_jump(lineno)
-        self.done.set()
 
     def Interrupt(self):
-        if self.pipe:
+        if self.attached and self.interacting == False:
             self.interrupt()
 
     def LoadBreakpoints(self):
@@ -319,75 +296,18 @@ class Debugger(qdb.Frontend):
             for lineno, (temporary, cond) in bps.items():
                 print "loading breakpoint", filename, lineno
                 self.do_set_breakpoint(filename, lineno, temporary, cond)
-                self.push_actions()
 
-    def async_push(self, action):
-        # wait any pending command
-        self.access.acquire()
-        # do not send GUI notifications (to not alter the focus)
-        self.post_event = False
-        # if no interaction yet, send an interrupt (step on the next stmt)
-        if not self.interacting.is_set():
-            self.interrupt()
-            cont = True
-        else:
-            cont = False
-        # wait for interaction and send the method request (signal we are done)
-        self.interacting.wait()        
-        action()
-        try:
-            ret = self.push_actions()
-            return ret
-        finally:
-            self.interacting.clear()
-            self.done.set()
-            # if interrupted, send a continue to resume
-            if cont:
-                self.interacting.wait()
-                self.do_continue()
-                self.done.set()
-
+    @force_interaction
     def SetBreakpoint(self, filename, lineno, temporary=0, cond=None):
-        if not self.mutex.acquire(False):
-            return
-        try:
-            # only send if debugger is connected and attached
-            if self.pipe and self.attached.is_set():
-                action = lambda: self.do_set_breakpoint(filename, lineno, temporary, cond)
-                self.async_push(action)
-                return True
-            else:
-                return False
-        finally:
-            self.mutex.release()
+        self.do_set_breakpoint(filename, lineno, temporary, cond)
 
+    @force_interaction
     def ClearBreakpoint(self, filename, lineno):
-        if not self.mutex.acquire(False):
-            return
-        try:
-            # only send if debugger is connected and attached
-            if self.pipe and self.attached.is_set():
-                action = lambda: self.do_clear_breakpoint(filename, lineno)
-                self.async_push(action)
-                return True
-            else:
-                return False
-        except KeyError, e:
-            print e
-        finally:
-            self.mutex.release()
+        self.do_clear_breakpoint(filename, lineno)
             
+    @force_interaction
     def ClearFileBreakpoints(self, filename):
-        if not self.mutex.acquire(False):
-            return
-        try:
-            action = self.do_clear_file_breakpoints(filename)
-            self.async_push(action)
-            return True
-        except KeyError, e:
-            print e
-        finally:
-            self.mutex.release()
+        self.do_clear_file_breakpoints(filename)
 
     # modal functions required by Eval (must not block):
     
@@ -404,10 +324,9 @@ class Debugger(qdb.Frontend):
             return dlg.GetValue() 
         dlg.Destroy()
         
+    @check_interaction
     def Eval(self, arg):
-        if self.pipe and self.attached.is_set():
-            if not self.mutex.acquire(False):
-                return u'*** debugger busy'
+        if self.pipe and self.attached:
             try:
                 old_write = self.write
                 old_readline = self.readline
@@ -415,31 +334,29 @@ class Debugger(qdb.Frontend):
                 self.write = self.modal_write
                 self.readline = self.modal_readline
                 # we need the result right now:
-                return self.async_push(lambda: self.do_eval(arg))
+                return self.do_eval(arg)
             except qdb.RPCError, e:
                 return u'*** %s' % unicode(e)
             finally:
                 self.write = old_write
                 self.readline = old_readline
-                self.mutex.release()
 
+    @check_interaction
     def ReadFile(self, filename):
         "Load remote file"
         from cStringIO import StringIO
-        action = lambda: self.do_read(filename)
-        data = self.async_push(action)
+        data = self.do_read(filename)
         return StringIO(data)
 
+    @check_interaction
     def GetContext(self):
         self.set_burst(3)
-        self.do_where()
-        w = self.push_actions()
+        w = self.do_where()
         ret = []
         for filename, lineno, bp, current, source in w:
             ret.append((filename, lineno, "%s%s" % (bp, current), source))
         d = {'call_stack': ret}
-        self.do_environment()
-        env = self.push_actions()
+        env = self.do_environment()
         ret = ""
         d['environment'] = env
         return d
@@ -448,54 +365,51 @@ class Debugger(qdb.Frontend):
     
     def Exec(self, statement, write=None, readline=None):
         "Exec source code statement in debugger context (returns string)"
-        if self.pipe and self.attached.is_set():
-            if self.mutex.acquire(False):
-                old_write = self.write
-                old_readline = self.readline
-                try:
-                    # replace console function
-                    if write:
-                        self.write = write
-                    if readline:
-                        self.readline = readline
-                    # execute the statement in the remote debugger:
-                    ret = self.async_push(lambda: self.do_exec(statement))
-                    if isinstance(ret, basestring):
-                        return ret
-                    else:
-                        return str(ret)
-                except qdb.RPCError, e:
-                    return u'*** %s' % unicode(e)
-                finally:
-                    self.write = old_write
-                    self.readline = old_readline
-                    self.mutex.release()
+        if statement == "" or not self.attached:
+            # 1. shell seems to call Exec without statement on init
+            # 2. if not debuging, exec on the current local wx shell
+            pass  
+        elif not self.interacting:
+            wx.Bell()
+            return u'*** no debugger interaction (stop first!)'
+        else:
+            old_write = self.write
+            old_readline = self.readline
+            try:
+                # replace console function
+                if write:
+                    self.write = write
+                if readline:
+                    self.readline = readline
+                # execute the statement in the remote debugger:
+                ret = self.do_exec(statement)
+                if isinstance(ret, basestring):
+                    return ret
+                else:
+                    return str(ret)
+            except qdb.RPCError, e:
+                return u'*** %s' % unicode(e)
+            finally:
+                self.write = old_write
+                self.readline = old_readline
         return None
 
     def GetAutoCompleteList(self, expr=''):
         "Return list of auto-completion options for an expression"
-        if self.pipe and self.attached.is_set():
-            if self.mutex.acquire(False):
-                try:
-                    cmd = lambda: self.get_autocomplete_list(expr)
-                    return self.async_push(cmd)
-                except qdb.RPCError, e:
-                    return u'*** %s' % unicode(e)
-                finally:
-                    self.mutex.release()
+        if self.pipe and self.attached and self.interacting:
+            try:
+                return self.get_autocomplete_list(expr)
+            except qdb.RPCError, e:
+                return u'*** %s' % unicode(e)
 
     def GetCallTip(self, expr):
         "Returns (name, argspec, tip) for an expression"
-        if self.pipe and self.attached.is_set():
-            if self.mutex.acquire(False):
-                try:
-                    cmd = lambda: self.get_call_tip(expr)
-                    return self.async_push(cmd)
-                except qdb.RPCError, e:
-                    return u'*** %s' % unicode(e)
-                finally:
-                    self.mutex.release()
-
+        if self.pipe and self.attached and self.interacting:
+            try:
+                return self.get_call_tip(expr)
+            except qdb.RPCError, e:
+                return u'*** %s' % unicode(e)
+    
 
 class EnvironmentPanel(wx.Panel):
     def __init__(self, parent=None):
