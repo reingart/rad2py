09ae4df9bb9335794698e3e50ed76534e44cff2b
Mariano Reingart <reingart@gmail.com>
2014-10-29 00:31:57 -0300

    Add basic query support to database API

--- ide2py/db.py
+++ ide2py/db.py
@@ -17,6 +17,7 @@ class Database():
     
     def __init__(self, path, **kwargs):
         self.cnn = sqlite3.connect(path)
+        self.cnn.row_factory = sqlite3.Row
         self.primary_keys = {}
     
     def create(self, table, _auto=True, **fields):
@@ -79,14 +80,44 @@ class Database():
         self.cnn.commit()
         return cur.rowcount
 
+    def select(self, table, **kwargs):
+        "Query rows (filter by given values)"
+        items = kwargs.items()
+        basic_types = int, str, float
+        aggregate_types = {sum: 'sum', len: 'count', min: 'min', max: 'max'}
+        all_types = basic_types + tuple(aggregate_types.keys())
+        where = ' AND '.join(["%s=?" % k for k, v in items
+                                         if v not in all_types])
+        fields = ', '.join([(k if v in basic_types 
+                               else "%s(%s)" % (aggregate_types[v], k))
+                            for k, v in items 
+                            if v in all_types])
+        group_by = ', '.join([k for k, v in items 
+                                if not v in (aggregate_types.keys())])
+        values = [v for k, v in items if v not in all_types]
+        sql = "SELECT %s FROM %s" % (fields, table)
+        if where:
+            sql += " WHERE %s" % where
+        if group_by:
+            sql += " GROUP BY %s" % group_by
+        cur = self.cnn.cursor()
+        print sql, values
+        cur.execute(sql, values)
+        self.cnn.commit()
+        return cur.fetchall()
+
 
 if __name__ == "__main__":
     db = Database(path="test.db")
-    db.create("t1", t1_id=int, f=float, s=str)
+    t1 = db.create("t1", t1_id=int, f=float, s=str)
     db.create("t2", t2_id=int, f=float, s=str, t1_id=int)
     id1 = db.insert("t1", f=3.14159265359, s="pi")
+    id1 = db.insert("t1", f=2.71828182846, s="e")
     id2 = db.insert("t2", f=2.71828182846, s="e", t1_id=id1)
     ok = db.update("t1", t1_id=id1, s="PI")
     assert ok > 0
     ok = db.delete("t2", f=2.71828182846, s="e")
     assert ok > 0
+
+    rows = db.select('t1', f=sum, s="pi")
+    print rows[0]["sum(f)"]
